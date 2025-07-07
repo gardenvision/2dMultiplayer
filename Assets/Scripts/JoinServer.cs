03f451b39f92d59359f4c3ab045f693bd4901f4c
Graden <7436517+gardenvision@users.noreply.github.com>
2025-07-07 17:00:53 +0200

    init commit
    Init commit

---
+++ Assets/Scripts/JoinServer.cs
@@ -0,0 +1,30 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class JoinServer : MonoBehaviour
+{
+    public void Join(){ 
+        // This method will be called when the player clicks the "Join" button
+        Debug.Log("Join button clicked. Implement server joining logic here.");
+        
+        // Example: Load a new scene or connect to a server
+        // SceneManager.LoadScene("GameScene");
+         NetworkManager.Singleton.StartClient();
+    }
+    
+    private void Start()
+    {
+        NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+        NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnected;
+    }
+
+    private void OnClientConnected(ulong clientId)
+    {
+        Debug.Log($"Client {clientId} connected");
+    }
+
+    private void OnClientDisconnected(ulong clientId)
+    {
+        Debug.Log($"Client {clientId} disconnected");
+    }
+}
